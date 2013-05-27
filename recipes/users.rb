70c975210a8316b5246b331c927dc206f5216712
Dr Nic Williams <drnicwilliams@gmail.com>
2013-05-26 21:44:08 -0700

    Use the attribute for the vcap username

--- recipes/users.rb
+++ recipes/users.rb
@@ -1,8 +1,8 @@
-group "vcap"
+group node.bosh_agent.username
 
-user "vcap" do
-  group "vcap"
-  home "/home/vcap"
+user node.bosh_agent.username do
+  group node.bosh_agent.username
+  home "/home/#{node.bosh_agent.username}"
   system true
   shell "/bin/bash"
   supports({ manage_home: true })
