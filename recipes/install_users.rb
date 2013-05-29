d39ee49b7492f67a9a1ea5b6bab9cf349945c48a
Dr Nic Williams <drnicwilliams@gmail.com>
2013-05-28 18:17:20 -0700

    assign node.bosh_user.password to users [fixes #4]

--- recipes/install_users.rb
+++ recipes/install_users.rb
@@ -14,12 +14,24 @@ node.bosh_user.groups.split(",").each do |grp|
   end
 end
 
+# echo \"vcap:${bosh_users_password}\" | chpasswd
+# echo \"root:${bosh_users_password}\" | chpasswd
+
+execute "set root password" do
+  command "echo \"root:#{node.bosh_user.password}\" | chpasswd"
+  action :run
+end
+
+execute "set node.bosh_user.username password" do
+  command "echo \"#{node.bosh_user.username}:#{node.bosh_user.password}\" | chpasswd"
+  action :run
+end
+
 execute "add #{node.bosh_user.username} user to groups" do
   command "usermod -a -G #{node.bosh_user.groups} #{node.bosh_user.username}"
   user "root"
   action :run
 end
-
 
 execute "add vagrant user to #{node.bosh_user.username}" do
   command "usermod -a -G #{node.bosh_user.username} vagrant"
